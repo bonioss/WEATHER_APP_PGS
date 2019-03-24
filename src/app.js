14c8653ce646b8b5ac87a4f04066a8af17ad0807
bonioss <slash98.pb@gmail.com>
2019-03-25 00:01:25 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import {convert} from './convertTemperature.js'
+import {
+  convert
+} from './convertTemperature.js'
 
 const apiKey = "64aff70137bc9abe67c79efb84133f54";
 const form = document.querySelector('#city-form');
@@ -13,53 +15,52 @@ function loadEventListeners() {
   document.addEventListener('DOMContentLoaded', getCitiesFromLocalStorage);
   form.addEventListener('submit', addCityToList);
   cityList.addEventListener('click', removeButton);
-  document.addEventListener('keypress', function(event){
+  document.addEventListener('keypress', function (event) {
     if (event.keyCode === 13 || event.which === 13) {
-        addCityToList(event);
+      addCityToList(event);
     } else {
-        
+
     }
-    
 
 
-  });  
+
+  });
 }
 
-function apiRequest(city, element){
+function apiRequest(city, element) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
   let results;
   //const td = document.createElement('td');
 
-   function procesRequest () {
-     var temperature;
-     var humidity;
-     var pressure;
+  function procesRequest() {
+    var temperature;
+    var humidity;
+    var pressure;
 
-     const t = document.createElement('p');
-     const h = document.createElement('p');
-     const p = document.createElement('p');
+    const t = document.createElement('p');
+    const h = document.createElement('p');
+    const p = document.createElement('p');
     axios.get(url)
       .then((response) => {
         console.log(response);
-        if(response.status===200){
-          temperature = response.data.main.temp;
-          humidity = response.data.main.humidity;
-          pressure = response.data.main.pressure;
-          t.innerHTML = "Temperature: "+ convert(temperature.toString()) + "&degC";
-          h.innerHTML = "Humidity: "+humidity+"%";
-          p.innerHTML = "Pressure: "+pressure+" hPa";
-          element.appendChild(t);
-          element.appendChild(h);
-          element.appendChild(p);
-          return;
-        } else {
-          console.log("not working");
-        }
- 
+
+        temperature = response.data.main.temp;
+        humidity = response.data.main.humidity;
+        pressure = response.data.main.pressure;
+        t.innerHTML = "Temperature: " + convert(temperature.toString()) + "&degC";
+        h.innerHTML = "Humidity: " + humidity + "%";
+        p.innerHTML = "Pressure: " + pressure + " hPa";
+        element.appendChild(t);
+        element.appendChild(h);
+        element.appendChild(p);
+        return;
       })
-      //.catch((error) => {
-        //console.log(error);
-      //});
+      .catch((error) => {
+        if (null !== error.request && 404 !== error.request.status) {
+          console.log(error);
+        }
+
+      });
   }
 
   procesRequest();
@@ -67,97 +68,96 @@ function apiRequest(city, element){
 }
 
 //get cities from localStorage
-function getCitiesFromLocalStorage(){
+function getCitiesFromLocalStorage() {
   let cities;
   //var domCollection = '.collection-item';
-  if(localStorage.getItem('cities') === null){
+  if (localStorage.getItem('cities') === null) {
     cities = [];
   } else {
     cities = JSON.parse(localStorage.getItem('cities'));
   }
 
-  cities.forEach(function(city){
-   
+  cities.forEach(function (city) {
+
     const li = document.createElement('li');
     li.className = 'collection-item';
-        //create clouds icon
-       const link2 = document.createElement('i');
-        link2.className = 'material-icons';
-        link2.innerHTML = 'filter_drama';
-        li.appendChild(link2);
-        
+    //create clouds icon
+    const link2 = document.createElement('i');
+    link2.className = 'material-icons';
+    link2.innerHTML = 'filter_drama';
+    li.appendChild(link2);
+
     //create text node and append to li
     li.appendChild(document.createTextNode(city));
-   
+
     //create new link createElement
     const link = document.createElement('a');
-    
+
     //create new p element
     apiRequest(city, li);
-    
+
     link.className = 'delete-item secondary-content';
     link.innerHTML = '<i class="material-icons">delete</i>';
     //append the link to li
     li.appendChild(link);
-   
+
     //append li to ul
     cityList.appendChild(li);
   });
 
 }
 //add city by button
-  function addCityToList(e) {
-    var checkCity = validateCity(citySearch.value);
+function addCityToList(e) {
+  var checkCity = validateCity(citySearch.value);
   if (citySearch.value === '') {
     //change this to toast
     alert("Enter city name");
-  }else if(checkCity){
+  } else if (checkCity) {
     return;
-  } 
-  else {
+  } else {
     //create li element
 
-  
-        const li = document.createElement('li');
-        li.className = 'collection-item';
-        const link2 = document.createElement('i');
-        link2.className = 'material-icons';
-        link2.innerHTML = 'filter_drama';
-        li.appendChild(link2);
-        
-        //create text node and append to li
-        li.appendChild(document.createTextNode(" " + citySearch.value.toUpperCase()));
-        //create new link createElement
-        
-         if(apiRequest(citySearch.value, li)!==""){
-          //apiRequest(citySearch.value, li);
-        const link = document.createElement('a');
 
-        link.className = 'delete-item secondary-content';
-        //add icon html
-        link.innerHTML = '<i class="material-icons">delete</i>';
-        //append the link to li
-        li.appendChild(link);
-        //append li to ul
-        cityList.appendChild(li);
-        // store in localStorage
-        var cityLocal = " " + citySearch.value.toUpperCase();
-       storeInLocalStorage(cityLocal);
-        //clear input
-        citySearch.value="";
-         } else{
-          var deleteChild = document.querySelector('collection');
-          deleteChild.removeChild(deleteChild.lastChild);  
-         }
-            
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    const link2 = document.createElement('i');
+    link2.className = 'material-icons';
+    link2.innerHTML = 'filter_drama';
+    li.appendChild(link2);
+
+    //create text node and append to li
+    li.appendChild(document.createTextNode(" " + citySearch.value.toUpperCase()));
+    //create new link createElement
+
+    if (apiRequest(citySearch.value, li) !== "") { //to dodalem zeby nie dodawalo glupot, ale nie dziala
+      //apiRequest(citySearch.value, li);
+      const link = document.createElement('a');
+
+      link.className = 'delete-item secondary-content';
+      //add icon html
+      link.innerHTML = '<i class="material-icons">delete</i>';
+      //append the link to li
+      li.appendChild(link);
+      //append li to ul
+      cityList.appendChild(li);
+      // store in localStorage
+      var cityLocal = " " + citySearch.value.toUpperCase();
+      storeInLocalStorage(cityLocal);
+      //clear input
+      citySearch.value = "";
+    } else {
+      var deleteChild = document.querySelector('collection');
+      deleteChild.removeChild(deleteChild.lastChild);
+    }
+
+  }
+  e.preventDefault();
+
 }
-e.preventDefault();
 
-}
-
-function storeInLocalStorage(city){
+function storeInLocalStorage(city) {
   let cities;
-  if(localStorage.getItem('cities') === null){
+  if (localStorage.getItem('cities') === null) {
     cities = [];
   } else {
     cities = JSON.parse(localStorage.getItem('cities'));
@@ -166,18 +166,18 @@ function storeInLocalStorage(city){
   localStorage.setItem('cities', JSON.stringify(cities));
 }
 
-function removeFromLocalStorage(cityItem){
+function removeFromLocalStorage(cityItem) {
   let cities;
-  if(localStorage.getItem('cities') === null){
+  if (localStorage.getItem('cities') === null) {
     cities = [];
   } else {
     cities = JSON.parse(localStorage.getItem('cities'));
   }
 
-  cities.forEach(function(city, index){
+  cities.forEach(function (city, index) {
     //delete is a name of an icon and idk why (recurrence?) but its appended to city name
-    if(cityItem.childNodes[1].textContent === city){
-      cities.splice(index,1);
+    if (cityItem.childNodes[1].textContent === city) {
+      cities.splice(index, 1);
     }
   });
 
@@ -193,15 +193,17 @@ function removeButton(e) {
   }
   e.preventDefault();
 }
-function validateCity(searchCity){
+
+function validateCity(searchCity) {
   var bool = false;
   searchCity = " " + searchCity.toUpperCase();
   console.log(searchCity);
-  
-  var storage = localStorage.cities.split('"');
-  for(var i=0; i<storage.length; i++){
-    if(searchCity===storage[i]){
-      bool=true;
+  if (localStorage.getItem("cities") !== null) {
+    var storage = localStorage.cities.split('"');
+    for (var i = 0; i < storage.length; i++) {
+      if (searchCity === storage[i]) {
+        bool = true;
+      }
     }
   }
   return bool;
